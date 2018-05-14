ee99c30aed233c99aa33f663663386b3537cb1fc
Brian Hobbs <brianhobbs216@gmail.com>
2018-05-14 10:31:19 -0400

    created nav component

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import '../home.css';
 
 class Home extends Component {
  
   render() {
     return (
-      <div>
-        <h2>Home Page</h2>
+      <div className=" container-fluid header">
+        <h2 className="text-center">Brian Hobbs</h2>
+        <p className="text-center p-text">Full Stack Developer</p>
       </div>
     );
   }
---
+++ src/components/mainnav.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import Home from './home';
+import About from './about';
+import Portfolio from './portfolio';
+import Contact from './contact';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+   } from 'reactstrap';
+
+   class Mainnav extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+ 
+    render() {
+      return (
+        <div>
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Brian Hobbs</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/portfolio">Portfolio</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact">Contact</NavLink>
+          </NavItem>
+            
+          </Nav>
+        </Collapse>
+      </Navbar>
+  
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/portfolio" component={Portfolio}/>
+      <Route path="/contact" component={Contact}/>
+        </div>
+      );
+    }
+  }
+  
+  export default Mainnav;
