f1c01b12add81981c526456ec99bbe95d1423a1d
Brian Hobbs <brianhobbs216@gmail.com>
2018-05-17 11:50:04 -0400

    restructured about page

--- src/components/about.js
+++ src/components/about.js
@@ -14,21 +14,65 @@ class About extends Component {
 
           <Row>
             <Col md="4">
+        
+
               <div className="team-member">
-                <img src="../pic_of_me.jpg" className="img-responsive img-thumbnail profile-img"></img>
-                <h4 className="profile-name">Brian Hobbs</h4>
-                <p className="text-muted">Full Stack Developer</p>
- 
+              <h4 className="profile-name">Who am I?</h4>
+              <p className="text-muted">I'm an ABA Therapist turned Full-Stack Web Developer. I special in rails backend with react frontend. </p>
+              
+              
               </div>
+              <div className="team-member">
+              <h4>My services</h4>
+              <ListGroup>
+              <ListGroupItem>Freelance</ListGroupItem>
+              <ListGroupItem>Front-end Development</ListGroupItem>
+              <ListGroupItem>Back-end Development</ListGroupItem>
+              <ListGroupItem>Mobile</ListGroupItem>
+              <ListGroupItem>Wordpress</ListGroupItem>
+              <ListGroupItem>Responsive Design</ListGroupItem>
+            </ListGroup>
+              
+              </div>
+         
             </Col>
+         
+          
+        
+
+         
             <Col md="8">
-              <div className="team-member-two">
-              <h2 className="profile-lead">A little about me<span className="dots">...</span></h2>
-              <p className="profile-about">I'm a former ABA Therapist turned Full Stack Web developer</p>
-              </div>
+        
+            <div className="skills">
+      <div className="text-center desc-bar">Javascript</div>
+      <Progress value="60" />
+      <div className="text-center desc-bar">Ruby</div>
+      <Progress value="70" />
+      <div className="text-center desc-bar">Ruby on Rails</div>
+      <Progress value="80" />
+      <div className="text-center desc-bar">React</div>
+      <Progress value="40" />
+      <div className="text-center desc-bar">React Native</div>
+      <Progress value="30" />
+      <div className="text-center desc-bar">Bootstrap</div>
+      <Progress value="65" />
+      <div className="text-center desc-bar">HTML5/CSS3/SCSS</div>
+      <Progress value="88" />
+      <div className="text-center desc-bar">Postgresql</div>
+      <Progress value="65" />
+      <div className="text-center desc-bar">Coffeescript</div>
+      <Progress value="25" />
+      <div className="text-center desc-bar">Angular5</div>
+      <Progress value="37" />
+      
+    </div>
+
+   
+
             </Col>
           
-          </Row>
+            </Row>
+         
           
     
 
