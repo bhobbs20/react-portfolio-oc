45002eb3927f6d4f57a34099fb13e9acb89fb40a
Brian Hobbs <brianhobbs216@gmail.com>
2018-05-17 13:42:44 -0400

    changed structure of contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,13 +8,17 @@ class Contact extends Component {
  
   render() {
     return (
-      <div className="contact">
+      <div className="contact container">
+
+      <h1 className="contact-title text-center">CONTACT</h1>
+     <p className="text-center contact-p">Whether you're interested in working with me or just want to say hello, I'd love to hear from you!</p>
 
       <div className="container contact-form">
-      <h3 className="text-center">Contact Me</h3>
-
-      <Form>
+     
+      <Row>
       
+      <Col lg="8">
+      <Form>
         <FormGroup>
           <Input type="name" name="name" id="contactName" placeholder="Name"/>
         </FormGroup>
@@ -31,7 +35,25 @@ class Contact extends Component {
         </FormGroup>
         <Button>Submit</Button>
       </Form>
-      
+      </Col>
+      <Col className="contact-details" lg="4">
+      <div>
+        <h4>Location</h4>
+        <p className="text-muted">Georgetown, MA 01833</p>
+
+        <h4>Phone</h4>
+        <p className="text-muted">978-500-4457</p>
+
+        <h4>Email</h4>
+        <p className="text-muted">brianhobbs216@gmail.com</p>
+
+        <h4>Social</h4>
+        <p className="text-muted">github twitter</p>
+      </div>
+
+      </Col>
+
+      </Row>
       </div>
        
 
